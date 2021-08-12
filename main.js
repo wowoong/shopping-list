bacae1287bfe7496b6c628c78cb68d56f1145fd0
wowoong <hyenyoung92@naver.com>
2021-08-12 11:14:21 +0900

    upgrade

--- main.js
+++ main.js
@@ -21,37 +21,23 @@ function onAdd() {
   input.focus();
 }
 
-
+let id = 0;
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
-
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
-
-  const name = document.createElement('span');
-  name.setAttribute('class', 'item__name');
-  name.innerText = text;
-
-  const button = document.createElement('button');
-  button.setAttribute('class', 'item__delete');
-  button.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  button.addEventListener('click', () => {
-    items.removeChild(itemRow)
-  });
-
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'divider');
-
-
-  item.appendChild(name);
-  item.appendChild(button);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.setAttribute('data-id', id)
+  itemRow.innerHTML = `
+    <div class="item">
+      <span class="item__name">${text}</span>
+      <button class="item__delete">
+        <i class="fas fa-trash-alt" data-id=${id}></i>
+      </button>
+    </div>
+    <div class="divider"></div>
+  `;
+  id++;
   return itemRow;
 }
-
 
 addBtn.addEventListener('click', () => {
   onAdd();
@@ -62,3 +48,11 @@ input.addEventListener('keypress', (event) => {
     onAdd();
   }
 });
+
+items.addEventListener('click', event => {
+  const id = event.target.dataset.id;
+  if(id) {
+    const deleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    deleted.remove();
+  }
+;})
